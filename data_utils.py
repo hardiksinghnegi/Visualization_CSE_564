052e6f5aee17bda85e1005980b39ec79e0341a4a
hardiksinghnegi <hardiksinghnegi@gmail.com>
2019-04-27 16:35:03 -0400

    Add Scree and refactoring

--- data_utils.py
+++ data_utils.py
@@ -18,12 +18,21 @@ def get_year_bar_data(data):
 
 
 def get_incidents_per_year(data):
-    return data['Year'].value_counts().to_dict()
+    csv_df = data[['Year']]
+    csv_df['Year'] = pd.to_datetime(csv_df['Year'], format="%Y")
+    tmp = csv_df.groupby('Year').size()
+    df = tmp.to_frame()
+    df = tmp.reset_index(name = 'count')
+    df['Year'] = pd.to_datetime(df["Year"]).dt.strftime('%-d-%b-%Y')
+    df.rename(columns={'Year': 'date'}, inplace=True)
+    print(df)
+    df.to_csv('static/data_files/csv/scree_index.csv', index=False)
+    df = data['Year'].value_counts().to_dict()
 
 
-# config = configparser.ConfigParser()
-# config.read('config.ini')
-# shooting_dataset = config['DATA']['INPUT_CSV_1']
-# df = prepare_data(shooting_dataset)
+config = configparser.ConfigParser()
+config.read('config.ini')
+shooting_dataset = config['DATA']['INPUT_CSV_1']
+df = prepare_data(shooting_dataset)
 # get_year_bar_data(df)
-# year_dict = get_incidents_per_year(df)
+year_dict = get_incidents_per_year(df)
